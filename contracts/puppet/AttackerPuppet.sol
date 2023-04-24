a689ada10da9e6cc5d04de5d9f3d9202f6f939e7
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2023-04-23 23:55:41 -0300

    attempt 1

--- contracts/puppet/AttackerPuppet.sol
+++ contracts/puppet/AttackerPuppet.sol
@@ -4,10 +4,64 @@ pragma solidity ^0.8.0;
 import "../DamnValuableToken.sol";
 
 contract AttackerPuppet {
-    function getMessage(
+    function transfer (
         address token,
-        uint value,
-        uint deadline
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        ERC20(token).permit(
+            msg.sender,
+            address(this),
+            value,
+            deadline,
+            v,
+            r,
+            s
+        );
+    }
+
+    function recover (
+        address token,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external view returns (address) {
+        bytes32 domain = ERC20(token).DOMAIN_SEPARATOR();
+        uint nonce = ERC20(token).nonces(msg.sender);
+        return ecrecover(
+            keccak256(
+                abi.encodePacked(
+                    "\x19\x01",
+                    domain,
+                    keccak256(
+                        abi.encode(
+                            keccak256(
+                                "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
+                            ),
+                            msg.sender,
+                            address(this),
+                            value,
+                            nonce,
+                            deadline
+                        )
+                    )
+                )
+            ),
+            v,
+            r,
+            s
+        );
+    }
+
+    function message (
+        address token,
+        uint256 value,
+        uint256 deadline
     ) external view returns (bytes memory) {
         bytes32 domain = ERC20(token).DOMAIN_SEPARATOR();
         uint nonce = ERC20(token).nonces(msg.sender);
@@ -29,12 +83,39 @@ contract AttackerPuppet {
         );
     }
 
-    function recover(
-        bytes32 message,
+    function messageHash (
+        address token,
+        uint256 value,
+        uint256 deadline
+    ) external view returns (bytes32) {
+        bytes32 domain = ERC20(token).DOMAIN_SEPARATOR();
+        uint nonce = ERC20(token).nonces(msg.sender);
+        return keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                domain,
+                keccak256(
+                    abi.encode(
+                        keccak256(
+                            "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
+                        ),
+                        msg.sender,
+                        address(this),
+                        value,
+                        nonce,
+                        deadline
+                    )
+                )
+            )
+        );
+    }
+
+    function recover2(
+        bytes32 _hash,
         uint8 v,
         bytes32 r,
         bytes32 s
     ) external pure returns (address) {
-        return ecrecover(message, v, r, s);
+        return ecrecover(_hash, v, r, s);
     }
 }
--- test/puppet/puppet.challenge.js
+++ test/puppet/puppet.challenge.js
@@ -4,7 +4,6 @@ const factoryJson = require("../../build-uniswap-v1/UniswapV1Factory.json");
 const { ethers } = require('hardhat');
 const { expect } = require('chai');
 const { setBalance } = require("@nomicfoundation/hardhat-network-helpers");
-const { keccak256 } = require("@ethersproject/keccak256");
 
 // Calculates how much ETH (in wei) Uniswap will pay for the given amount of tokens
 function calculateTokenToEthInputPrice(tokensSold, tokensInReserve, etherInReserve) {
@@ -137,29 +136,106 @@ describe('[Challenge] Puppet', function () {
         //
         // await getReport();
       
+        const domain = await token.DOMAIN_SEPARATOR();
+        
+        const spender = ethers.utils.getContractAddress({
+            from: player.address,
+            nonce: 0
+        });
+
+        const hash_selector = ethers.utils.keccak256(
+            ethers.utils.hexlify(
+            ethers.utils.toUtf8Bytes(
+                "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
+        )));
+
+        const message_1 = ethers.utils.defaultAbiCoder.encode([
+            "bytes32",
+            "address",
+            "address",
+            "uint256",
+            "uint256",
+            "uint256"
+        ],[
+            hash_selector,
+            player.address,
+            spender,
+            PLAYER_INITIAL_TOKEN_BALANCE,
+            0,
+            deadline
+        ]);
+        
+        const hash_message_1 = ethers.utils.keccak256(message_1);
+
+        const message = ethers.utils.solidityPack([
+            "string",
+            "bytes32",
+            "bytes32"
+        ],[
+            "\x19\x01",
+            domain,
+            hash_message_1
+        ]);
+
+        const signature = await player.signMessage(message);
+        const split = ethers.utils.splitSignature(signature);
+        
         const Attacker = await ethers.getContractFactory('AttackerPuppet', player);
         const attacker = await Attacker.deploy();
-
-        const message = await attacker.getMessage(
+        const message_correct = await attacker.message(
             token.address,
             PLAYER_INITIAL_TOKEN_BALANCE,
             deadline
         );
 
-        const messageBytes = ethers.utils.arrayify(message);
-        const messageHash = ethers.utils.hashMessage(messageBytes);
-        
-        const messageHashBytes = ethers.utils.arrayify(messageHash);
-        // const signature = await player.signMessage(messageHashBytes);
-        const signature = await player.signMessage(messageBytes);
-        const recoveredAddress = ethers.utils.verifyMessage(messageBytes, signature);
-        console.log("player  :", player.address);
-        console.log("recover1:", recoveredAddress);
+        console.log();
+        console.log('Correct');
+        console.log(message_correct);
 
-        const split = ethers.utils.splitSignature(signature);
-        const actualSigner = await attacker.recover(messageHash, split.v, split.r, split.s);
-        console.log("recover2:", actualSigner);
+        console.log();
+        console.log('Mine');
+        console.log(message);
 
+        const correctHash = await attacker.messageHash(
+            token.address,
+            PLAYER_INITIAL_TOKEN_BALANCE,
+            deadline
+        );
+        console.log();
+        console.log('hash correct');
+        console.log(correctHash);
+
+        const mineHash = ethers.utils.keccak256(message);
+        console.log();
+        console.log('hash mine');
+        console.log(mineHash);
+
+        const recover_out = ethers.utils.verifyMessage(message, signature);
+        const recover_1 = await attacker.recover(
+            token.address,
+            PLAYER_INITIAL_TOKEN_BALANCE,
+            deadline,
+            split.v,
+            split.r,
+            split.s,
+            {
+                gasLimit: 1000000
+            }
+        );
+        const recover_2 = await attacker.recover2(
+            mineHash,
+            split.v,
+            split.r,
+            split.s
+        );
+        const recoverLixo = ethers.utils.verifyMessage(message, split);
+
+        console.log();
+        console.log(`Player     : ${player.address}`);
+        console.log(`Recover out: ${recover_out}`);
+        console.log(`Recover 1  : ${recover_1}`);
+        console.log(`Recover 2  : ${recover_2}`);
+        console.log(`Recover lix: ${recoverLixo}`);
     });
 
     after(async function () {
